f07726a3ab07bf1c6262a4d1e850abfab9872a96
LucasRobs <lucasrobs@alu.ufc.br>
2021-10-20 00:30:00 -0300

    Show real location

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Map } from './src/page/Map';
+import React, { useEffect, useState } from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  SafeAreaView,
+} from "react-native";
+import { Map } from "./src/page/Map";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [showMap, setShowMap] = useState(false);
+
+  useEffect(() => {
+    Location.installWebGeolocationPolyfill();
+  }, []);
   return (
-    // <View style={styles.container}>
-      <Map />
-    // </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.buttonOpenMap}
+          onPress={() => setShowMap(!showMap)}
+        >
+          <Text style={styles.buttonText}>
+            {showMap ? "Parar transmissão" : "Iniciar transmissão"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {showMap ? <Map /> : <View />}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  container: {
+    height: 200,
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonOpenMap: {
+    padding: 5,
+    marginTop: 10,
+    borderRadius: 5,
+    backgroundColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%",
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
--- src/page/Map/index.tsx
+++ src/page/Map/index.tsx
@@ -1,32 +1,91 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
 import MapView, { MarkerAnimated } from "react-native-maps";
-
-const REGION = {
-  latitude: -3.3484309,
-  longitude: -39.1389627,
-  latitudeDelta: 0,
-  longitudeDelta: 0,
+interface RegionProps {
+  latitude: Number;
+  longitude: Number;
+  latitudeDelta: Number;
+  longitudeDelta: Number;
 }
 
 export function Map() {
-  const [region, setRegion] = useState(REGION);
+  const [latitude, setLatitude] = useState(-4.9790697);
+  const [longitude, setLongitude] = useState(-39.055975);
+  const [latitudeDelta, setLatitudeDelta] = useState(0.005);
+  const [longitudeDelta, setLongitudeDelta] = useState(0);
+  const [watchID, setWatchID] = useState(0);
+  useEffect(() => {}, []);
+
+  function callLocation() {
+    if (Platform.OS === "ios") {
+      getLocation();
+    } else {
+      const requestLocationPermission = async () => {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Permissão de Acesso à Localização",
+            message: "Este aplicativo precisa acessar sua localização.",
+            buttonNeutral: "Pergunte-me depois",
+            buttonNegative: "Cancelar",
+            buttonPositive: "Aceitar",
+          }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getLocation();
+        } else {
+          alert("Permissão de Localização negada");
+        }
+      };
+      requestLocationPermission();
+    }
+  }
+
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const currentLatitude = +JSON.stringify(position.coords.latitude);
+        const currentLongitude = +JSON.stringify(position.coords.longitude);
+        setLatitude(currentLatitude);
+        setLongitude(currentLongitude);
+      },
+      (error) => alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    const watchID = navigator.geolocation.watchPosition(
+      (position) => {
+        const currentLatitude = +JSON.stringify(position.coords.latitude);
+        const currentLongitude = +JSON.stringify(position.coords.longitude);
+        setLatitude(currentLatitude);
+        setLongitude(currentLongitude);
+      },
+      () => {}
+    );
+    setWatchID(watchID);
+  }
+
+  const clearLocation = () => {
+    navigator.geolocation.clearWatch(watchID);
+  };
+
+  useEffect(() => {
+    callLocation();
+  }, []);
 
   return (
-    <MapView 
-      region={region}
-      style={{
-        flex: 1
-      }}
-      onRegionChange={(region) => setRegion(region)}
+    <MapView
+      region={{ latitude, longitude, latitudeDelta, longitudeDelta }}
+      style={{ flex: 1, backgroundColor: "#444" }}
+      onRegionChange={(region: RegionProps) => {}}
       zoomEnabled
       scrollDuringRotateOrZoomEnabled
     >
-      <MarkerAnimated 
-        coordinate={{ 
-          latitude: -3.3484309,
-          longitude: -39.1389627,
+      <MarkerAnimated
+        coordinate={{
+          latitude: latitude,
+          longitude: longitude,
         }}
-        onSelect={() => console.log('Lagoinha')}
+        onSelect={() => console.log("Lagoinha")}
       />
     </MapView>
   );
